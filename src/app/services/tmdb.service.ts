ec7d1f55632c63a07466cda42dcc56a721a781a2
Tiziano Occhionero <tiziano.occhionero@gmail.com>
2025-09-30 17:13:14 +0200

    fix2: in inserimento cerca anche le serie tv

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -9,18 +9,34 @@ export class TmdbService {
 
   constructor(private http: HttpClient) {}
 
+  // 🔹 Nuovo metodo unico per cercare film, serie, persone
   cerca(query: string): Observable<any> {
     const url = `${this.baseUrl}/search/multi?api_key=${this.apiKey}&query=${encodeURIComponent(query)}&language=it-IT`;
     return this.http.get<any>(url);
   }
 
+  // 🔹 Nuovo metodo dettagli (funziona per movie e tv)
   getDettagli(id: number, tipo: 'movie' | 'tv'): Observable<any> {
     const url = `${this.baseUrl}/${tipo}/${id}?api_key=${this.apiKey}&language=it-IT`;
     return this.http.get<any>(url);
   }
 
+  // 🔹 Nuovo metodo crediti (funziona per movie e tv)
   getCrediti(id: number, tipo: 'movie' | 'tv'): Observable<any> {
     const url = `${this.baseUrl}/${tipo}/${id}/credits?api_key=${this.apiKey}&language=it-IT`;
     return this.http.get<any>(url);
+  }
+
+  // ✅ Wrapper per compatibilità col vecchio codice
+  cercaFilm(query: string): Observable<any> {
+    return this.cerca(query);
+  }
+
+  getDettagliFilm(id: number): Observable<any> {
+    return this.getDettagli(id, 'movie');
+  }
+
+  getCreditiFilm(id: number): Observable<any> {
+    return this.getCrediti(id, 'movie');
   }
 }
